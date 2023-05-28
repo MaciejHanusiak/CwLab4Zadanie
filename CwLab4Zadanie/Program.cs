4932a9bfaab63ea65e3933865cfeb44aa5d702a5
macie <macie@DESKTOP-7HGICA3>
2023-05-28 22:56:46 +0200

    Add project files.

---
+++ CwLab4Zadanie/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CwLab4Zadanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Zadanie5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Copier copier = new Copier();
+            Console.WriteLine($"Power status: {copier.GetState()}");
+            copier.PowerOn();
+            Console.WriteLine($"Power status: {copier.GetState()}");
+            var doc = new PDFDocument("doc.pdf");
+            copier._Printer.Print(doc);
+            copier._Printer.Print(doc);
+            copier._Printer.Print(doc);
+            //cooling down after 3 prints
+            copier._Printer.Print(doc);
+            copier._Scanner.Scan();
+            copier._Scanner.Scan();
+            //cooling down after 2 scans
+            copier._Scanner.Scan();
+            copier.GetCounter();
+            Console.WriteLine($"Power status: {copier.GetState()}");
+            copier.PowerOff();
+            Console.WriteLine($"Power status: {copier.GetState()}");
+
+        }
+    }
+}
